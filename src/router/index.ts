9b949a9c660ecbf43f629154f6424560afad27d3
李威 <liweijsj@163.com>
2024-09-05 19:54:44 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory('/abc'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
