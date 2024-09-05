f68d36fbb711db6bb9bb5bfb274968dd6cdc3c6b
李威 <liweijsj@163.com>
2024-09-05 21:42:29 +0800

    使用腾讯TDesign的Button按钮

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="row">
-      <t-button size="large" theme="primary" variant="dashed">虚框按钮</t-button>
-    </div>
+  <div class="row">
+    <t-button @click="goToYouglish" size="large" theme="primary" variant="dashed">alligator【看视频】</t-button>
+    <t-button @click="goToBingImg" size="large" theme="primary" variant="dashed">alligator【看图片】</t-button>
+    <t-button @click="goToBaiduFanYi" size="large" theme="primary" variant="dashed">alligator【查词典】</t-button>
+  </div>
 </template>
 
 <style scoped lang="less">
@@ -20,4 +22,15 @@
 </style>
 
 <script setup lang="ts">
+function goToYouglish() {
+  window.open('https://youglish.com/pronounce/alligator/english', '_blank')
+}
+
+function goToBingImg() {
+  window.open('https://www.bing.com/images/search?q=alligator', '_blank')
+}
+
+function goToBaiduFanYi() {
+  window.open('https://fanyi.baidu.com/#en/zh/alligator', '_blank')
+}
 </script>
