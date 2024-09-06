4e27ed22f20752dddccab8d7ac60c1a2bd42fe38
李威 <liweijsj@163.com>
2024-09-06 15:20:30 +0800

    pnpm create vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import './assets/main.css'
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// Plugins
+import { registerPlugins } from '@/plugins'
 
-import TDesign from 'tdesign-mobile-vue'
-
+// Components
 import App from './App.vue'
-import router from './router'
 
-import 'tdesign-mobile-vue/es/style/index.css'
+// Composables
+import { createApp } from 'vue'
 
 const app = createApp(App)
 
-app.use(TDesign)
-app.use(createPinia())
-app.use(router)
+registerPlugins(app)
 
 app.mount('#app')
---
+++ src/pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-layout>
+    <v-app-bar color="teal-lighten-3" comfortable>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My ABC</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <template v-if="$vuetify.display.mdAndUp">
+        <v-btn icon="mdi-magnify" variant="text"></v-btn>
+        <v-btn icon="mdi-filter" variant="text"></v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :location="$vuetify.display.mobile ? 'bottom' : undefined"
+      temporary
+    >
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
+
+    <v-main style="height: 800px;">
+      <v-card-text>
+        <v-btn variant="outlined">Button</v-btn>
+      </v-card-text>
+    </v-main>
+
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+    items: [
+      {
+        title: 'Foo',
+        sum: 666,
+        value: 'foo'
+      },
+      {
+        title: 'Bar',
+        sum: 888,
+        value: 'bar'
+      },
+      {
+        title: 'Fizz',
+        sum: 999,
+        value: 'fizz'
+      },
+      {
+        title: 'Buzz',
+        sum: 444,
+        value: 'buzz'
+      }
+    ]
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false
+    }
+  }
+}
+</script>
