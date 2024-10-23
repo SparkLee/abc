2fe4e2f4fd62f38692b8fdcbe8ecb6f9de345176
李威 <liweijsj@163.com>
2024-10-23 09:39:47 +0800

    Vite环境变量的TypeScript智能提示：https://cn.vitejs.dev/guide/env-and-mode.html#intellisense

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_SERVER_PORT: string
+  readonly VITE_API_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
